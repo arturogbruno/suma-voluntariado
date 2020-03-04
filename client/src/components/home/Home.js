ae7eaa7f163f8ee333d96e5f3a1c0ba0cc55fe32
Arturo G. Bruno <arturogbruno@gmail.com>
2020-03-05 00:43:07 +0100

    data visualization test in front

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,28 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ActivitiesServices from '../../services/activities';
+
 
 export default class Home extends React.Component {
     constructor(props) {
       super(props);
   
       this.state = {
-        loggedInUser: null
+        loggedInUser: null,
+        allActivities: []
       };
   
       this.activitiesServices = new ActivitiesServices();
     }
 
-    componentDidMount = () => this.getAllActivities();
-
     getAllActivities = () => {
         this.activitiesServices.getAllActivities()
-            .then(allActivities => this.setState({ activities: allActivities }))
+            .then(allActivities => this.setState({ allActivities: allActivities.allActivities }))
             .catch(err => console.log(err))
     }
 
+    componentDidMount = () => this.getAllActivities();
+
     render() {
         return(
-            <h1>Estás en la Home</h1>
+            <div>
+                <h1>Estás en la Home</h1>
+                {this.state.allActivities.length ? (
+                    <ul>
+                        {this.state.allActivities.map(activity => <li><Link to={`/activities/${activity._id}`} key={activity._id}>{activity.title}</Link></li>)}
+                    </ul>
+                )
+                    :
+                    <p>CARGANDO...</p>
+                }
+            </div>
         )
     }
 
