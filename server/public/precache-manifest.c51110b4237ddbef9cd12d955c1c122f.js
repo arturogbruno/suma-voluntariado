3b485da2515cdeb00ceeeaee15d40778bc9cf78f
Arturo G. Bruno <arturogbruno@gmail.com>
2020-03-05 16:06:20 +0100

    first deploy to heroku

---
+++ server/public/precache-manifest.c51110b4237ddbef9cd12d955c1c122f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b122ee634547457c2ae0cfcf95904a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c0d48aa9bbb6a6cb4f",
+    "url": "/static/css/main.3a0967c2.chunk.css"
+  },
+  {
+    "revision": "49558bbcaad32cd0af53",
+    "url": "/static/js/2.66b1a0af.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.66b1a0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c0d48aa9bbb6a6cb4f",
+    "url": "/static/js/main.6be494b8.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
