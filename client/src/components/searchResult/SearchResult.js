63144e79c8e917060fa8af25f8279253d995392f
Arturo G. Bruno <arturogbruno@gmail.com>
2020-03-11 18:06:37 +0100

    add filters for activities

--- client/src/components/searchResult/SearchResult.js
+++ client/src/components/searchResult/SearchResult.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import Spinner from 'react-bootstrap/Spinner';
 import ActivitiesServices from '../../services/activities';
 import ActivityOverview from '../activityOverview/ActivityOverview';
