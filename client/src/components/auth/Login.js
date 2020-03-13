9812048f9172837fb2658c8a786b2776b95ab3ad
Arturo G. Bruno <arturogbruno@gmail.com>
2020-03-13 13:33:24 +0100

    add styles for login and signup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import AuthServices from '../../services/auth';
-import './Auth.scss';
+import './Login.scss';
 
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            username: 'arturo',
-            password: 'arturo'
+            username: '',
+            password: ''
         }
 
         this.authServices = new AuthServices()
@@ -38,34 +38,29 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <Col xs={{span: 3, offset: 2}} className="loginForm">
-                        <h1>¡Nos encanta que vuelvas!</h1>
-                        <Form onSubmit={this.handleSubmit}>
-                            <Form.Group> 
-                                <Form.Label>Usuario:</Form.Label>
-                                <Form.Control type="text" name="username" value={this.state.username} onChange={this.handleChange} required />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Contraseña:</Form.Label>
-                                <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-                            </Form.Group>
-                            <Form.Group className="loginBtn">
-                                <Button type="submit">Iniciar sesión</Button>
-                            </Form.Group>
-                        </Form>
-                        <div className="signupLnk">
-                            <span className="lnk">¿Aún no tienes una cuenta? <Link to="/signup">Regístrate</Link></span>
-                            <h1>{this.state.error ? 'Error' : ''}</h1>
-                        </div>
-                    </Col>
+            <div className="login">
+                <div className="loginForm">
+                    <h1>¡Nos encanta que vuelvas!</h1>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group> 
+                            <Form.Label>Usuario:</Form.Label>
+                            <Form.Control type="text" name="username" value={this.state.username} onChange={this.handleChange} required />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Contraseña:</Form.Label>
+                            <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+                        </Form.Group>
+                        <Form.Group className="loginBtn">
+                            <Button type="submit">Iniciar sesión</Button>
+                        </Form.Group>
+                    </Form>
+                </div>
 
-                    <Col xs={{span: 5, offset: 2}}>
-                        <img className="loginImg" src="https://images.unsplash.com/photo-1578357078586-491adf1aa5ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" alt=""/>
-                    </Col>
-                </Row>
-            </Container>
+                <div>
+                    <span className="lnk">¿Aún no tienes una cuenta? <Link to="/signup">Regístrate</Link></span>
+                    <h1>{this.state.error ? 'Error' : ''}</h1>
+                </div>
+            </div>
         )
     }
 }
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import AuthServices from '../../services/auth';
-import './Auth.scss';
+import './Signup.scss';
 
 export default class Signup extends React.Component {
     constructor(props) {
@@ -40,46 +40,41 @@ export default class Signup extends React.Component {
 
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <Col xs={{span: 3, offset: 2}} className="signupForm">
-                        <h1>¡Únete y suma!</h1>
-                        <Form onSubmit={this.handleSubmit}>
-                            <Form.Group> 
-                                <Form.Label>Soy:</Form.Label>
-                                    <Form.Control as="select" name="role" value={this.state.role} onChange={this.handleChange} required >
-                                        <option value="" disabled defaultValue>Selecciona tu rol</option>
-                                        <option value="volunteer">Voluntario</option>
-                                        <option value="organization">Organización</option>
-                                    </Form.Control>
-                            </Form.Group>
-                            <Form.Group> 
-                                <Form.Label>Email:</Form.Label>
-                                <Form.Control type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-                            </Form.Group>
-                            <Form.Group> 
-                                <Form.Label>Usuario:</Form.Label>
-                                <Form.Control type="text" name="username" value={this.state.username} onChange={this.handleChange} required />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Contraseña:</Form.Label>
-                                <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-                            </Form.Group>
-                            <Form.Group className="signupBtn">
-                                <Button type="submit">Registrarse</Button>
-                            </Form.Group>
-                        </Form>
-                        <div className="loginLnk">
-                            <span>¿Ya tienes una cuenta? <Link to="/login">Inicia sesión</Link></span>
-                            <h1>{this.state.error ? 'Error' : ''}</h1>
-                        </div>
-                    </Col>
+            <div className="signup">
+                <div className="signupForm">
+                    <h1>¡Únete y suma!</h1>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group> 
+                            <Form.Label>Soy:</Form.Label>
+                                <Form.Control as="select" name="role" value={this.state.role} onChange={this.handleChange} required >
+                                    <option value="" disabled defaultValue>Selecciona tu rol</option>
+                                    <option value="volunteer">Voluntario</option>
+                                    <option value="organization">Organización</option>
+                                </Form.Control>
+                        </Form.Group>
+                        <Form.Group> 
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+                        </Form.Group>
+                        <Form.Group> 
+                            <Form.Label>Usuario:</Form.Label>
+                            <Form.Control type="text" name="username" value={this.state.username} onChange={this.handleChange} required />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Contraseña:</Form.Label>
+                            <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+                        </Form.Group>
+                        <Form.Group className="signupBtn">
+                            <Button type="submit">Registrarse</Button>
+                        </Form.Group>
+                    </Form>
+                </div>
 
-                    <Col xs={{span: 5, offset: 2}}>
-                        <img className="loginImg" src="https://images.unsplash.com/photo-1578357078586-491adf1aa5ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" alt=""/>
-                    </Col>
-                </Row>
-            </Container>
+                <div>
+                    <span>¿Ya tienes una cuenta? <Link to="/login">Inicia sesión</Link></span>
+                    <h1>{this.state.error ? 'Error' : ''}</h1>
+                </div>
+            </div>
         )
     }
 }
