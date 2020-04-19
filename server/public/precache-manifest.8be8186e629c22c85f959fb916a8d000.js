0bf66bed2aad9399cbf3113325a45561395e746b
Arturo G. Bruno <arturogbruno@gmail.com>
2020-04-19 21:45:25 +0200

    fix images

---
+++ server/public/precache-manifest.8be8186e629c22c85f959fb916a8d000.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a648ba132cd0c6efda8b191623bc135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6c253590a7e35ce78b9",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "da5f026140b6668c89e4",
+    "url": "/static/css/main.116a757d.chunk.css"
+  },
+  {
+    "revision": "f6c253590a7e35ce78b9",
+    "url": "/static/js/2.0bd8f4f7.chunk.js"
+  },
+  {
+    "revision": "acc9932894342abd62bf4897376ce383",
+    "url": "/static/js/2.0bd8f4f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5f026140b6668c89e4",
+    "url": "/static/js/main.8e190ce3.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
